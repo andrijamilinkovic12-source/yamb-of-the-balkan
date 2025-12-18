c15e38d8381692634e15f1cda13f2f8a942374e0
andrijamilinkovic12-source <andrija.milinkovic12@gmail.com>
2025-12-19 00:08:21 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -6,107 +6,97 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-    cors: {
-        origin: "*", // Dozvoli pristup sa svih adresa (bitno za Render)
-        methods: ["GET", "POST"]
-    }
+    cors: { origin: "*", methods: ["GET", "POST"] }
 });
 
-// Serviraj statički HTML fajl
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-let waitingPlayer = null; // Igrač koji čeka protivnika
+// --- LOGIKA IGRE ---
+
+let waitingPlayer = null; // Za "Random" matchmaking
 
 io.on('connection', (socket) => {
-    console.log('Igrač konektovan:', socket.id);
+    console.log('Konektovan:', socket.id);
 
-    // 1. TRAŽENJE PROTIVNIKA
+    // --- 1. NASUMIČNA IGRA (Random Matchmaking) ---
     socket.on('find_game', (nickname) => {
         socket.nickname = nickname;
-
         if (waitingPlayer && waitingPlayer.id !== socket.id) {
-            // Imamo par!
-            const opponent = waitingPlayer;
-            waitingPlayer = null; // Resetuj čekaonicu
-
-            const roomId = "room_" + opponent.id + "_" + socket.id;
-            
-            // Ubaci oba igrača u sobu
-            socket.join(roomId);
-            opponent.join(roomId);
-
-            // Sačuvaj ID sobe kod oba igrača
-            socket.roomId = roomId;
-            opponent.roomId = roomId;
-
-            // Pokreni igru: Opponent je Prvi (index 0), Socket je Drugi (index 1)
-            io.to(opponent.id).emit('game_start', { 
-                myIndex: 0, 
-                opponent: socket.nickname, 
-                roomId: roomId 
-            });
-            
-            io.to(socket.id).emit('game_start', { 
-                myIndex: 1, 
-                opponent: opponent.nickname, 
-                roomId: roomId 
-            });
-
-            console.log(`Start igre: ${opponent.nickname} vs ${socket.nickname} u sobi ${roomId}`);
-
-        } else {
-            // Nema nikog, ti čekaš
-            waitingPlayer = socket;
-            console.log(`${nickname} čeka u redu...`);
-        }
-    });
-
-    // 2. SINHRONIZACIJA BACANJA KOCKICA
-    socket.on('dice_roll', (data) => {
-        // data: { roomId, values, bacanje }
-        socket.to(data.roomId).emit('remote_roll', data);
-    });
-
-    // 3. SINHRONIZACIJA ZADRŽAVANJA (HOLD)
-    socket.on('dice_hold', (data) => {
-        // data: { roomId, index, status }
-        socket.to(data.roomId).emit('remote_hold', data);
-    });
-
-    // 4. SINHRONIZACIJA NAJAVE
-    socket.on('announce', (data) => {
-        // data: { roomId, type } (start/cancel)
-        socket.to(data.roomId).emit('remote_announce', data);
-    });
-
-    // 5. SINHRONIZACIJA POTEZA (UPIS)
-    socket.on('player_move', (data) => {
-        // data: { roomId, row, col, points }
-        socket.to(data.roomId).emit('remote_move', data);
-    });
-
-    // 6. CHAT
-    socket.on('chat_msg', (data) => {
-        socket.to(data.roomId).emit('chat_msg', data);
-    });
-
-    // 7. DISKONEKCIJA
-    socket.on('disconnect', () => {
-        console.log('Diskonektovan:', socket.id);
-        
-        // Ako je izašao onaj koji čeka, isprazni čekaonicu
-        if (waitingPlayer === socket) {
+            startGame(waitingPlayer, socket);
             waitingPlayer = null;
+        } else {
+            waitingPlayer = socket;
+        }
+    });
+
+    // --- 2. PRIVATNA IGRA (Pozovi Prijatelja) ---
+    socket.on('join_private_game', (data) => {
+        // data: { nickname, roomId }
+        socket.nickname = data.nickname;
+        const roomId = data.roomId;
+
+        // Proveri koliko ljudi ima u toj sobi
+        const room = io.sockets.adapter.rooms.get(roomId);
+        const numClients = room ? room.size : 0;
+
+        if (numClients === 0) {
+            // Soba je prazna, ti si Domaćin (Čekaš)
+            socket.join(roomId);
+            socket.roomId = roomId;
+            socket.emit('private_waiting', { roomId: roomId });
+            console.log(`Kreirana privatna soba: ${roomId} od strane ${data.nickname}`);
+        } 
+        else if (numClients === 1) {
+            // Soba ima jednog igrača, ti si Gost (Igra počinje)
+            socket.join(roomId);
+            socket.roomId = roomId;
+            
+            // Pronađi protivnika u toj sobi
+            const [firstPlayerId] = room; 
+            const opponentSocket = io.sockets.sockets.get(firstPlayerId);
+            
+            if (opponentSocket) {
+                startGame(opponentSocket, socket, roomId);
+            }
+        } 
+        else {
+            // Soba je puna
+            socket.emit('room_full');
+        }
+    });
+
+    // --- ZAJEDNIČKA FUNKCIJA ZA START ---
+    function startGame(player1, player2, specificRoomId = null) {
+        const roomId = specificRoomId || ("room_" + player1.id + "_" + player2.id);
+        
+        if (!specificRoomId) {
+            player1.join(roomId);
+            player2.join(roomId);
         }
 
-        // Ako je bio u igri, javi protivniku
-        if (socket.roomId) {
-            socket.to(socket.roomId).emit('opponent_left');
-        }
+        player1.roomId = roomId;
+        player2.roomId = roomId;
+
+        io.to(player1.id).emit('game_start', { myIndex: 0, opponent: player2.nickname, roomId: roomId });
+        io.to(player2.id).emit('game_start', { myIndex: 1, opponent: player1.nickname, roomId: roomId });
+
+        console.log(`Igra startovana: ${player1.nickname} vs ${player2.nickname} (Soba: ${roomId})`);
+    }
+
+    // --- PROSLEĐIVANJE POTEZA (Isto kao pre) ---
+    socket.on('dice_roll', (data) => socket.to(data.roomId).emit('remote_roll', data));
+    socket.on('dice_hold', (data) => socket.to(data.roomId).emit('remote_hold', data));
+    socket.on('announce', (data) => socket.to(data.roomId).emit('remote_announce', data));
+    socket.on('player_move', (data) => socket.to(data.roomId).emit('remote_move', data));
+    socket.on('chat_msg', (data) => socket.to(data.roomId).emit('chat_msg', data));
+
+    socket.on('disconnect', () => {
+        if (waitingPlayer === socket) waitingPlayer = null;
+        if (socket.roomId) socket.to(socket.roomId).emit('opponent_left');
     });
 });
 
