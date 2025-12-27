2cdcad47aee363f1e0a20db70bac4cfcf2d74c86
andrijamilinkovic12-source <andrija.milinkovic12@gmail.com>
2025-12-28 00:26:33 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -15,14 +15,46 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// --- LOGIKA IGRE ---
+// --- GLOBAL VARIABLES ---
+let waitingPlayer = null; // Za Random Matchmaking
+let globalHighscores = []; // Globalna Top Lista (u memoriji)
 
-let waitingPlayer = null; // Za "Random" matchmaking
+// Dummy podaci za početak (opciono)
+// globalHighscores = [
+//    { name: "YambMaster", score: 1100, mode: "Solo", date: "01.01.2025" }
+// ];
 
 io.on('connection', (socket) => {
     console.log('Konektovan:', socket.id);
 
-    // --- 1. NASUMIČNA IGRA (Random Matchmaking) ---
+    // --- A. TOP LISTA LOGIKA ---
+    
+    // 1. Slanje liste igraču kada zatraži
+    socket.on('get_global_highscores', () => {
+        socket.emit('global_highscores_data', globalHighscores);
+    });
+
+    // 2. Prijem novog rezultata
+    socket.on('submit_score', (data) => {
+        // data: { name, score, mode, date }
+        if (!data.name || !data.score) return;
+
+        // Dodaj novi skor
+        globalHighscores.push(data);
+
+        // Sortiraj (od najvećeg ka najmanjem)
+        globalHighscores.sort((a, b) => b.score - a.score);
+
+        // Zadrži samo top 50
+        globalHighscores = globalHighscores.slice(0, 50);
+
+        // (Opciono) Odmah javi svima da se lista promenila
+        // io.emit('global_highscores_data', globalHighscores);
+    });
+
+    // --- B. MULTIPLAYER LOGIKA ---
+
+    // 1. Random Matchmaking
     socket.on('find_game', (nickname) => {
         socket.nickname = nickname;
         if (waitingPlayer && waitingPlayer.id !== socket.id) {
@@ -33,61 +65,42 @@ io.on('connection', (socket) => {
         }
     });
 
-    // --- 2. PRIVATNA IGRA (Pozovi Prijatelja) ---
+    // 2. Privatna Igra
     socket.on('join_private_game', (data) => {
-        // data: { nickname, roomId }
         socket.nickname = data.nickname;
         const roomId = data.roomId;
-
-        // Proveri koliko ljudi ima u toj sobi
         const room = io.sockets.adapter.rooms.get(roomId);
         const numClients = room ? room.size : 0;
 
         if (numClients === 0) {
-            // Soba je prazna, ti si Domaćin (Čekaš)
             socket.join(roomId);
             socket.roomId = roomId;
             socket.emit('private_waiting', { roomId: roomId });
-            console.log(`Kreirana privatna soba: ${roomId} od strane ${data.nickname}`);
-        } 
-        else if (numClients === 1) {
-            // Soba ima jednog igrača, ti si Gost (Igra počinje)
+        } else if (numClients === 1) {
             socket.join(roomId);
             socket.roomId = roomId;
-            
-            // Pronađi protivnika u toj sobi
             const [firstPlayerId] = room; 
             const opponentSocket = io.sockets.sockets.get(firstPlayerId);
-            
-            if (opponentSocket) {
-                startGame(opponentSocket, socket, roomId);
-            }
-        } 
-        else {
-            // Soba je puna
+            if (opponentSocket) startGame(opponentSocket, socket, roomId);
+        } else {
             socket.emit('room_full');
         }
     });
 
-    // --- ZAJEDNIČKA FUNKCIJA ZA START ---
     function startGame(player1, player2, specificRoomId = null) {
         const roomId = specificRoomId || ("room_" + player1.id + "_" + player2.id);
-        
         if (!specificRoomId) {
             player1.join(roomId);
             player2.join(roomId);
         }
-
         player1.roomId = roomId;
         player2.roomId = roomId;
 
         io.to(player1.id).emit('game_start', { myIndex: 0, opponent: player2.nickname, roomId: roomId });
         io.to(player2.id).emit('game_start', { myIndex: 1, opponent: player1.nickname, roomId: roomId });
-
-        console.log(`Igra startovana: ${player1.nickname} vs ${player2.nickname} (Soba: ${roomId})`);
     }
 
-    // --- PROSLEĐIVANJE POTEZA (Isto kao pre) ---
+    // --- C. IGRA I CHAT ---
     socket.on('dice_roll', (data) => socket.to(data.roomId).emit('remote_roll', data));
     socket.on('dice_hold', (data) => socket.to(data.roomId).emit('remote_hold', data));
     socket.on('announce', (data) => socket.to(data.roomId).emit('remote_announce', data));
