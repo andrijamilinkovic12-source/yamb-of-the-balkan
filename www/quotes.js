521bacf398f1e7d6706132638573a8856b31506a
Andrija <andrija.milinkovic12@gmail.com>
2026-03-22 00:05:49 +0100

    Update 1 22.03.2026.

--- www/quotes.js
+++ www/quotes.js
@@ -1,22 +1,104 @@
-const quotesDb = [
-    {
-        sr: { text: "Mašta je važnija od znanja.", author: "Albert Ajnštajn" },
-        en: { text: "Imagination is more important than knowledge.", author: "Albert Einstein" }
+{
+        sr: { text: "Nikad nemoj odustati, jer uvek postoji vreme i mesto kada će se plima promeniti.", author: "Herijet Bičer Stou" },
+        en: { text: "Never give up, for that is just the place and time that the tide will turn.", author: "Harriet Beecher Stowe" }
     },
     {
-        sr: { text: "Nisam propao. Samo sam pronašao 10.000 načina koji ne rade.", author: "Tomas Edison" },
-        en: { text: "I have not failed. I've just found 10,000 ways that won't work.", author: "Thomas Edison" }
+        sr: { text: "Kreativan čovek motivisan je željom da postigne, a ne željom da pobedi druge.", author: "Ajn Rand" },
+        en: { text: "A creative man is motivated by the desire to achieve, not by the desire to beat others.", author: "Ayn Rand" }
     },
     {
-        sr: { text: "Ako želite da otkrijete tajne univerzuma, razmišljajte u terminima energije, frekvencije i vibracije.", author: "Nikola Tesla" },
-        en: { text: "If you want to find the secrets of the universe, think in terms of energy, frequency and vibration.", author: "Nikola Tesla" }
+        sr: { text: "Ama, misli dobro, pa će dobro i biti.", author: "Ivo Andrić" },
+        en: { text: "Just think well, and all will be well.", author: "Ivo Andrić" }
     },
     {
-        sr: { text: "Nauka u svakodnevnom životu ne sme biti samo za naučnike, već za sve ljude.", author: "Marija Kiri" },
-        en: { text: "Science in everyday life must not be just for scientists, but for all people.", author: "Marie Curie" }
+        sr: { text: "Neka ti udica bude uvek bačena. U jezeru u kojem najmanje očekuješ, pojaviće se riba.", author: "Ovidije" },
+        en: { text: "Let your hook always be cast. In the pool where you least expect it, there will be a fish.", author: "Ovid" }
     },
     {
-        sr: { text: "Sreća prati hrabre.", author: "Aleksandar Veliki" },
-        en: { text: "Fortune favors the bold.", author: "Alexander the Great" }
+        sr: { text: "Velika je nesreća kad čovek ne zna šta hoće, a prava katastrofa kad ne zna šta može.", author: "Jovan Dučić" },
+        en: { text: "It is a great misfortune when a man does not know what he wants, and a true disaster when he does not know what he can do.", author: "Jovan Dučić" }
+    },
+    {
+        sr: { text: "Odreknite se života sa samosažaljevanjem u zamenu za život sa svrhom.", author: "Nik Vujičić" },
+        en: { text: "Give up a life of self-pity in exchange for a life of purpose.", author: "Nick Vujicic" }
+    },
+    {
+        sr: { text: "Jedno ljudsko biće je jedan od 40.000 spermatozoida. Taj jedan je uspeo. 39.999 nisu uspeli. Znači, vi ste šampioni. Svako ko se rodio on je već uspeo.", author: "Zoran Đinđić" },
+        en: { text: "A human being is one out of 40,000 spermatozoa. That one succeeded. 39,999 failed. That means you are champions. Everyone who is born has already succeeded.", author: "Zoran Đinđić" }
+    },
+    {
+        sr: { text: "Za dvadeset godina, bićete više razočarani stvarima koje niste učinili nego stvarima koje jeste. Zato isplovite iz sigurne luke. Istražujte. Sanjajte. Otkrijte.", author: "Mark Tven" },
+        en: { text: "Twenty years from now you will be more disappointed by the things that you didn't do than by the ones you did do. So throw off the bowlines. Sail away from the safe harbor. Explore. Dream. Discover.", author: "Mark Twain" }
+    },
+    {
+        sr: { text: "Većinu važnih stvari u svetu postigli su ljudi koji nisu odustajali kada se činilo da uopšte nema nade.", author: "Dejl Karnegi" },
+        en: { text: "Most of the important things in the world have been accomplished by people who have kept on trying when there seemed to be no hope at all.", author: "Dale Carnegie" }
+    },
+    {
+        sr: { text: "Preko noći postaje slavan samo onaj ko je danima neumorno radio.", author: "Henri Ford" },
+        en: { text: "Only he who has worked tirelessly for days becomes famous overnight.", author: "Henry Ford" }
+    },
+    {
+        sr: { text: "Krenite korak po korak. Ne morate videti čitavo stubište. Samo napravite prvi korak.", author: "Martin Luter King" },
+        en: { text: "Take the first step in faith. You don't have to see the whole staircase, just take the first step.", author: "Martin Luther King Jr." }
+    },
+    {
+        sr: { text: "Nema više spavanja. Vreme je za akciju. Spavaćete kada odete u penziju.", author: "Zoran Đinđić" },
+        en: { text: "No more sleeping. It is time for action. You will sleep when you retire.", author: "Zoran Đinđić" }
+    },
+    {
+        sr: { text: "Pitanje nije ko će da mi dozvoli, već ko će da me zaustavi.", author: "Ajn Rand" },
+        en: { text: "The question isn't who is going to let me; it's who is going to stop me.", author: "Ayn Rand" }
+    },
+    {
+        sr: { text: "Sve je dostižno! Nemoguće postoji samo kod ludaka.", author: "Napoleon Bonaparta" },
+        en: { text: "Everything is attainable! Impossible is a word to be found only in the dictionary of fools.", author: "Napoleon Bonaparte" }
+    },
+    {
+        sr: { text: "Svi vaši snovi mogu postati stvarnost – ukoliko imate hrabrosti da ih sledite.", author: "Volt Dizni" },
+        en: { text: "All our dreams can come true, if we have the courage to pursue them.", author: "Walt Disney" }
+    },
+    {
+        sr: { text: "Čovek koji premeće planine, započinje premetanjem kamenčića.", author: "Kineska poslovica" },
+        en: { text: "The man who moves a mountain begins by carrying away small stones.", author: "Chinese Proverb" }
+    },
+    {
+        sr: { text: "Ja ne verujem u pesimizam. Ako nešto ne ide onako kako mi želimo, idi napred. Ako misliš da će padati kiša, padaće.", author: "Klint Istvud" },
+        en: { text: "I don't believe in pessimism. If something doesn't come up the way you want, forge ahead. If you think it's going to rain, it will.", author: "Clint Eastwood" }
+    },
+    {
+        sr: { text: "Oluje čine da hrastovi puštaju dublje korenje.", author: "Džordž Herbert" },
+        en: { text: "Storms make oaks take deeper root.", author: "George Herbert" }
+    },
+    {
+        sr: { text: "Nije bitno ko započinje igru već ko je završava.", author: "Džon Vuden" },
+        en: { text: "It's not so important who starts the game but who finishes it.", author: "John Wooden" }
+    },
+    {
+        sr: { text: "Bolje je boriti se i gubiti bitku nego nikada se ne izboriti.", author: "Hegel" },
+        en: { text: "It is better to fight and lose the battle than never to fight at all.", author: "Georg Wilhelm Friedrich Hegel" }
+    },
+    {
+        sr: { text: "Bez muke se pesma ne ispoja, bez muke se sablja ne sakova!", author: "Petar Petrović Njegoš" },
+        en: { text: "Without toil, a song is not sung; without toil, a sword is not forged!", author: "Petar II Petrović-Njegoš" }
+    },
+    {
+        sr: { text: "Ko ima volje, ima i načina.", author: "Džordž Bernard Šo" },
+        en: { text: "Where there's a will, there's a way.", author: "George Bernard Shaw" }
+    },
+    {
+        sr: { text: "Pobeda je najslađa kada već okusite poraz.", author: "Malkolm Forbs" },
+        en: { text: "Victory is sweetest when you've known defeat.", author: "Malcolm Forbes" }
+    },
+    {
+        sr: { text: "Nikada ne smete odustati. Pobednici nikad ne odustaju, a ljudi koji odustaju nikad ne pobeđuju.", author: "Ted Tarner" },
+        en: { text: "You should never give up. Winners never quit, and quitters never win.", author: "Ted Turner" }
+    },
+    {
+        sr: { text: "Izgleda nemoguće, dokle god se ne završi.", author: "Nelson Mandela" },
+        en: { text: "It always seems impossible until it's done.", author: "Nelson Mandela" }
+    },
+    {
+        sr: { text: "Prvo te ignorišu, onda ti se smeju, onda te biju, i onda pobeđuješ.", author: "Mahatma Gandi" },
+        en: { text: "First they ignore you, then they laugh at you, then they fight you, then you win.", author: "Mahatma Gandhi" }
     }
-];
